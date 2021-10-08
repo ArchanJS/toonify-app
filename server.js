d1f2f56ac0ddbba328fded000014c51ff2a48848
Archan Banerjee <64304109+ArchanJS@users.noreply.github.com>
2021-10-08 13:18:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ require('dotenv').config({path:'./config.env'});
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const deepai = require('deepai');
 
 app.use(bodyParser.json({ limit: "50mb" }))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }))
 app.use(cors());
 app.use(morgan("dev"));
 
-deepai.setApiKey('196e6806-f01c-4674-a021-c4ba8a7fdcf9');
 const port = 8000;
 
 app.use('/api/auth',require('./routes/toonifyRoute'));
